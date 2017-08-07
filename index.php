42ed3fc6cd3cb7a46611ebeb50966fa3cd1ee3ef
Marty Bonacci <martybonacci@gmail.com>
2017-08-07 11:14:23 -0600

    padding around columns fix

--- index.php
+++ index.php
@@ -96,7 +96,7 @@
 				<div class="row">
 					<div class="col-md-3">
 						<img src="images/placeholder-image600x600.png" class="img-responsive img-circle" alt="placeholder">
-						p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
 						totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
 						dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
 						sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt?</p>
@@ -104,7 +104,7 @@
 					</div>
 					<div class="col-md-3">
 						<img src="images/placeholder-image600x600.png" class="img-responsive img-circle" alt="placeholder">
-						p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
 						totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
 						dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
 						sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt?</p>
@@ -112,7 +112,7 @@
 					</div>
 					<div class="col-md-3">
 						<img src="images/placeholder-image600x600.png" class="img-responsive img-circle" alt="placeholder">
-						p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
 						totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
 						dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
 						sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt?</p>
@@ -120,7 +120,7 @@
 					</div>
 					<div class="col-md-3">
 						<img src="images/placeholder-image600x600.png" class="img-responsive img-circle" alt="placeholder">
-						p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
 						totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
 						dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
 						sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt?</p>
