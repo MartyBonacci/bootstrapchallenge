a111fcbedc6e30d75d09e35b0a5674c475bc0cdc
Marty Bonacci <martybonacci@gmail.com>
2017-08-07 13:29:27 -0600

    added link to answer

--- index.php
+++ index.php
@@ -96,7 +96,7 @@
 						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
 							totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
 							dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
-							sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt?</p>
+							sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt?</p><a href="https://github.com/rlewis2892/bootstrap-challenge-answer">link to answer</a>
 					</div>
 				</div>
 			<div class="container-fluid">
